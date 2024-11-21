d93d825cb31aef3a679f9c367ce073f7ac03c628
mangala-karunarathne <mangalaicc@gmail.comm>
2024-11-21 21:59:51 +0530

    CHG | Server and Database Connection

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB Database Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+const app = express();
+app.listen(5000, () => console.log("Server listening on port 5000!"));
