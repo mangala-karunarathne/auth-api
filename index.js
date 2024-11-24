22405d18ce601e6444f5fcda4994425f80d8670f
mangala-karunarathne <mangalaicc@gmail.comm>
2024-11-24 21:34:48 +0530

    CHG | Add a Midlware and a Function to handle Errors

--- index.js
+++ index.js
@@ -22,3 +22,13 @@ app.listen(5000, () => console.log("Server listening on port 5000!"));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
